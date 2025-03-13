e2cf7fb98f703271feb96f611ff0ca7d65c7938d
sayan4545 <sayanchatterjee3010@gmail.com>
2025-03-13 23:55:04 +0530

    Added ResponseEntity class in the xcotyrolleer layer.

--- WebProject/src/main/java/com/devsayan/WebProject/controllers/EmployeeController.java
+++ WebProject/src/main/java/com/devsayan/WebProject/controllers/EmployeeController.java
@@ -3,6 +3,9 @@ package com.devsayan.WebProject.controllers;
 import com.devsayan.WebProject.dtos.EmployeeDTO;
 import com.devsayan.WebProject.services.EmployeeServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,30 +19,44 @@ public class EmployeeController {
 
     private final EmployeeServiceImpl employeeService;
     @GetMapping("/getAll")
-    public List<EmployeeDTO> getAllEmployees(){
-        return employeeService.getAll();
+    public ResponseEntity<List<EmployeeDTO>> getAllEmployees(){
+        return new ResponseEntity<>(employeeService.getAll(), HttpStatusCode.valueOf(200));
     }
     @PostMapping("/post")
-    public EmployeeDTO saveEmployeeDto(@RequestBody EmployeeDTO inputEmployeeDTO){
-        return employeeService.createEmployee(inputEmployeeDTO);
+    public ResponseEntity<EmployeeDTO> saveEmployeeDto(@RequestBody EmployeeDTO inputEmployeeDTO){
+        employeeService.createEmployee(inputEmployeeDTO);
+        return new ResponseEntity<>(inputEmployeeDTO, HttpStatus.CREATED);
     }
     @GetMapping("/getById/{id}")
-    public Optional<EmployeeDTO> findByID(@PathVariable Long id){
-        return employeeService.findEmployeeById(id);
+    public ResponseEntity<EmployeeDTO> findByID(@PathVariable Long id){
+        Optional<EmployeeDTO> employeeDTO = employeeService.findEmployeeById(id);
+        return employeeDTO.
+                map(employeeDTO1 -> ResponseEntity.ok(employeeDTO1))
+                .orElse(ResponseEntity.notFound()
+                        .build());
     }
     @PutMapping("/put/{id}")
-    public EmployeeDTO updateEmployee(@RequestBody EmployeeDTO employeeDTO,
+    public ResponseEntity<EmployeeDTO> updateEmployee(@RequestBody EmployeeDTO employeeDTO,
                                       @PathVariable Long id){
-        return employeeService.updateEmployeeById(employeeDTO,id);
+        employeeService.updateEmployeeById(employeeDTO,id);
+        return new ResponseEntity<>(employeeDTO,HttpStatus.OK);
     }
+//    @DeleteMapping("/delete/{id}")
+//    public boolean deleteById(@PathVariable Long id){
+//        return employeeService.deleteById(id);
+//
+//    }
     @DeleteMapping("/delete/{id}")
-    public boolean deleteById(@PathVariable Long id){
-        return employeeService.deleteById(id);
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        boolean isDeleted = employeeService.deleteById(id);
+        if (isDeleted) return ResponseEntity.noContent().build();
+        return ResponseEntity.notFound().build();
     }
     @PatchMapping("/patch/{id}")
-    public EmployeeDTO updatePartialEmployeeById(@RequestBody
+    public ResponseEntity<EmployeeDTO> updatePartialEmployeeById(@RequestBody
                                                  Map<String,Object> updates,
                                                  @PathVariable Long id){
-        return employeeService.updatePartialEmployeeById(updates,id);
+        EmployeeDTO updatedEmployee = employeeService.updatePartialEmployeeById(updates,id);
+        return ResponseEntity.ok(updatedEmployee);
     }
 }
